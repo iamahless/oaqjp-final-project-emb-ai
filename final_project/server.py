1ffd7a0bd33caf14b5e3014b189bf29d3a7f6e39
Alexander <alexandergaruba96@gmail.com>
2025-02-26 00:42:48 +0100

    Enhance Flask server with detailed docstrings and improved response formatting for emotion analysis

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,7 @@
+'''Deploy a Flask application that enables users to input
+a text string for analysis, identifying the most likely
+emotion from a set of five possible emotions.
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,21 +9,31 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
-     text_to_analyse = request.args.get('textToAnalyze')
-     emotion_result = emotion_detector(text_to_analyse)
-     anger = emotion_result['anger']
-     disgust = emotion_result['disgust']
-     fear = emotion_result['fear']
-     joy = emotion_result['joy']
-     sadness = emotion_result['sadness']
-     dominant_emotion = emotion_result['dominant_emotion']
+    '''Get the text input from the user, analyze it using the emotion detector,
+    and return a response showing the confidence scores for all emotions
+    along with the dominant emotion.
+    '''
+    text_to_analyse = request.args.get('textToAnalyze')
+    emotion_result = emotion_detector(text_to_analyse)
+    anger = emotion_result['anger']
+    disgust = emotion_result['disgust']
+    fear = emotion_result['fear']
+    joy = emotion_result['joy']
+    sadness = emotion_result['sadness']
+    dominant_emotion = emotion_result['dominant_emotion']
 
-     if dominant_emotion is None:
-          return "Invalid text! Please try again"
-     return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is <strong>{dominant_emotion}</strong>."
+    if dominant_emotion is None:
+        return "Invalid text! Please try again"
+    response_str = f"""For the given statement, the system response is
+    'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
+    The dominant emotion is <strong>{dominant_emotion}</strong>."""
+    return response_str
 
 @app.route("/")
 def render_index_page():
+    '''Display the index page where users can input a text string for
+    analysis and view the results.
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
